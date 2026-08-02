4c7be7a050477536a45b337056f382705d957b8e
lxpraveen <kingpraveen6589@gmail.com>
2026-08-02 18:33:50 +0530

    Fix MainLayout Navbar

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
 import Services from "../services/Services";
-//import Services from "../components/ServicesSection/ServicesSection";
 import About from "../pages/About/About";
 import Contact from "../pages/Contact/Contact";
 import Login from "../pages/Login/Login";
